c620384c2ef4106c7ef4f6b45d210082fac45bf7
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2023-08-23 21:40:02 +0700

    add writing page

---
+++ src/routes/writing/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import BlogCard from '$lib/components/blog-card.svelte';
+
+  export let data;
+  let posts = data.posts;
+</script>
+
+<svelte:head>
+  <title>Writing</title>
+</svelte:head>
+
+<div class="pb-44">
+  <h1 class="text-3xl md:text-5xl font-bold py-16 text-center">WRITING</h1>
+  <div class="pt-10">
+    <div class="grid grid-cols-1 gap-12">
+      {#each posts as post}
+        <BlogCard {post} />
+      {/each}
+    </div>
+  </div>
+</div>
