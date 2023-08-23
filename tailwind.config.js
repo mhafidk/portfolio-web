e2fe1bc38f32af89fc485d8dd6ea8cc532bd1588
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2023-08-23 11:44:32 +0700

    add hero index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Montserrat', '"Open Sans"', 'Roboto', 'Arial']
+      },
+    },
   },
   plugins: [],
 }
