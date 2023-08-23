4b4fc3c660eb1517bb51cbc0449ee7097bcc6246
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2023-08-23 17:18:05 +0700

    add experience section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import Navbar from "../lib/components/navbar.svelte";
 </script>
 
-<div class="bg-neutral-50 text-neutral-900">
+<div class="bg-neutral-50 text-neutral-900 font-medium">
   <div class="p-4 max-w-screen-md mx-auto">
     <Navbar />
     <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,72 @@
-<div class="py-32 text-xl">
+<svelte:head>
+	<title>Muhammad Hafid K — That's me!</title>
+</svelte:head>
+
+<div class="py-32 text-xl text-center">
   <h1 class="text-6xl font-extrabold">MUHAMMAD HAFID K</h1>
-  <p class="pt-2 font-medium">That's my name!</p>
+  <p class="pt-2 text-xl font-semibold">That's my name!</p>
   <p class="pt-2">Software engineer based in Bogor, Indonesia. Currently working as a backend engineer at <a href="https://www.qontak.com" class="text-pink-600 border-b border-pink-600">Qontak</a>.</p>
-  <div class="py-6 flex gap-10 text-violet-600">
+  <div class="py-6 flex justify-center gap-10 text-violet-600">
     <a href="mailto:mhafidk@gmail.com" target="_blank">
       <svg class="stroke-violet-600 hover:stroke-violet-700 w-8 h-8" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 9l5 3.5L17 9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2 17V7a2 2 0 012-2h16a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2z" stroke-width="1.5"></path></svg></a>
     <a href="https://github.com/mhafidk" target="_blank" rel="noreferrer">
       <svg class="stroke-violet-600 hover:stroke-violet-700 w-8 h-8" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M16 22.027v-2.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75 5.07 5.07 0 00-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 005 5.797a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58v2.87M9 20.027c-3 .973-5.5 0-7-3" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
     <a href="https://www.linkedin.com/in/mhafidk/" target="_blank" rel="noreferrer">
       <svg class="stroke-violet-600 hover:stroke-violet-700 w-8 h-8" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M21 8v8a5 5 0 01-5 5H8a5 5 0 01-5-5V8a5 5 0 015-5h8a5 5 0 015 5zM7 17v-7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M11 17v-3.25M11 10v3.75m0 0c0-3.75 6-3.75 6 0V17M7 7.01l.01-.011" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
+  </div>
+  <div class="pt-3 text-neutral-50 text-lg">
+    <a href="/about" class="px-4 py-2 bg-violet-600 hover:bg-violet-700 rounded">More about me</a>
+    <a href="/about" class="px-4 py-2 bg-pink-600 hover:bg-pink-700 rounded">Read my blog</a>
+  </div>
+</div>
+<div class="pb-32">
+  <h2 class="text-4xl font-bold border-b pb-2 text-center">EXPERIENCES</h2>
+  <p class="text-sm pt-2 text-neutral-400 text-center">My professional work journey</p>
+  <div class="pt-8">
+    <div class="flex gap-4">
+      <p class="text-violet-600 font-bold text-2xl">#1</p>
+      <div class="w-full">
+        <p class="font-bold text-2xl">
+          Backend Engineer, <a href="https://www.qontak.com" class="text-violet-600 underline"
+            >Mekari Qontak</a
+          >
+        </p>
+        <div class="text-lg mt-2">
+          <p>2020 - Present</p>
+          <div class="flex flex-wrap gap-2 mt-3 text-base items-center">
+            <p class="px-4 py-2 text-pink-600 border border-pink-600 rounded">
+              Ruby
+            </p>
+            <p class="px-4 py-2 text-pink-600 border border-pink-600 rounded">
+              Ruby on Rails
+            </p>
+            <p class="px-4 py-2 text-pink-600 border border-pink-600 rounded">
+              Go
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <p class="my-2 text-neutral-400 text-sm text-center mt-24">
+		list of tools I've been familiar with.
+	</p>
+	<div class="flex flex-wrap gap-2 justify-center items-center mt-4 text-base">
+		<p class="px-4 py-2 text-violet-600 border border-violet-600 rounded">Ruby</p>
+		<p class="px-4 py-2 text-violet-600 border border-violet-600 rounded">
+			Ruby on Rails
+		</p>
+		<p class="px-4 py-2 text-violet-600 border border-violet-600 rounded">Go</p>
+		<p class="px-4 py-2 text-violet-600 border border-violet-600 rounded">
+			JavaScript
+		</p>
+		<p class="px-4 py-2 text-violet-600 border border-violet-600 rounded">Svelte</p>
+	</div>
+</div>
+<div class="pb-32">
+  <h2 class="text-4xl font-bold border-b pb-2 text-center">PROJECTS</h2>
+  <p class="text-sm pt-2 text-neutral-400 text-center">My blood, sweat, and tears</p>
+  <div class="pt-8">
+
   </div>
 </div>
