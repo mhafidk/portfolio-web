3f12a87a1159b4a3e5ddfc9cd5a1ac07035858dc
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2023-08-23 21:34:10 +0700

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+  import "../app.css";
+  import { page } from '$app/stores';
+
+  let status = $page.status
+  let text = status == 404 ? "Page not found :(" : "There is something wrong..."
+</script>
+
+<svelte:head>
+  <title>There's something wrong :(</title>
+</svelte:head>
+
+<div class="h-screen w-screen bg-violet-600 text-neutral-50 flex justify-center items-center px-8 pt-8 pb-32">
+  <div class="grid grid-cols-1 px-4 gap-12">
+    <p class="text-center text-4xl md:text-6xl font-bold">{text}</p>
+    <div class="w-fit mx-auto">
+      <a href="/" class="bg-neutral-50 hover:bg-neutral-200 px-8 py-2 text-2xl md:text-3xl text-violet-600 font-bold rounded w-fit">Home</a>
+    </div>
+  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,21 @@
 <script>
   import "../app.css";
+  import { page } from '$app/stores';
+
 	import Footer from "../lib/components/footer.svelte";
 	import Navbar from "../lib/components/navbar.svelte";
+
+  let wip = ["projects"]
 </script>
 
-<div class="bg-neutral-50 text-neutral-900 font-medium">
-  <Navbar />
-  <div class="p-4 max-w-screen-md mx-auto">
-    <slot />
+{#if wip.includes($page.url.pathname.split("/")[1]) || ($page.error !== undefined && $page.error !== null)}
+  <slot />
+{:else}
+  <div class="bg-neutral-50 text-neutral-900 font-medium">
+    <Navbar />
+    <div class="p-4 max-w-screen-md mx-auto">
+      <slot />
+    </div>
+    <Footer />
   </div>
-  <Footer />
-</div>
+{/if}
