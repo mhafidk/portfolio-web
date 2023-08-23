e2a5fe005ed869a679426306e1c13c96ff75a097
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2023-08-23 19:29:48 +0700

    add project and writing section

---
+++ src/lib/components/blog-card.svelte
@@ -0,0 +1,9 @@
+<div class="">
+  <p class="text-3xl font-bold">What is Go and Why I Learned It?</p>
+  <p class="py-1 text-sm text-neutral-500">June 23, 2023</p>
+  <p class="line-clamp-3">Ever heard about Go or Golang? Go is one of the top, loved, and demanded programming languages out there. Here are the reasons why I learned Go. Should you learn Go?</p>
+  <div class="text-sm text-neutral-50 flex gap-2 pt-3">
+    <p class="bg-violet-600 px-4 py-1.5 rounded-full">Go</p>
+    <p class="bg-violet-600 px-4 py-1.5 rounded-full">Programming</p>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import BlogCard from "../lib/components/blog-card.svelte";
+
+</script>
 <svelte:head>
 	<title>Muhammad Hafid K — That's me!</title>
 </svelte:head>
@@ -67,6 +71,33 @@
   <h2 class="text-4xl font-bold border-b pb-2 text-center">PROJECTS</h2>
   <p class="text-sm pt-2 text-neutral-400 text-center">My blood, sweat, and tears</p>
   <div class="pt-8">
-
+    <div class="">
+      <img src="/portfolio-web.webp" alt="MHK" class="border rounded w-full h-full">
+      <div class="pt-4">
+        <h3 class="text-center text-2xl font-bold text-violet-600">Personal Website</h3>
+        <p class="pt-3 text-justify">My very own website and blog! I build this site using Svelte, Tailwind CSS, and Graph CMS. Frontend is not my strong suit, but this site isn't that bad, right? I learnt a lot about frontend while making this site. A lot of new features will come out!</p>
+        <div class="pt-3 flex justify-center gap-6 text-pink-600">
+          <p>Svelte</p>
+          <p>Tailwind CSS</p>
+          <p>Graph CMS</p>
+        </div>
+        <div class="flex justify-center gap-8 pt-5">
+          <a href="https://www.mhafidk.com"><svg class="stroke-pink-600 hover:stroke-pink-700 w-8 h-8" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M14 11.998C14 9.506 11.683 7 8.857 7H7.143C4.303 7 2 9.238 2 11.998c0 2.378 1.71 4.368 4 4.873a5.3 5.3 0 001.143.124" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M10 11.998c0 2.491 2.317 4.997 5.143 4.997h1.714c2.84 0 5.143-2.237 5.143-4.997 0-2.379-1.71-4.37-4-4.874A5.304 5.304 0 0016.857 7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
+        <a href="https://github.com/mhafidk/portfolio-web"><svg class="stroke-pink-600 hover:stroke-pink-700 w-8 h-8" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M16 22.027v-2.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75 5.07 5.07 0 00-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 005 5.797a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58v2.87M9 20.027c-3 .973-5.5 0-7-3" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="pb-32">
+  <h2 class="text-4xl font-bold border-b pb-2 text-center">WRITING</h2>
+  <p class="text-sm pt-2 text-neutral-400 text-center">Anything I've on my mind</p>
+  <div class="pt-8">
+    <div class="grid grid-cols-1 gap-12">
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+    </div>
   </div>
 </div>
