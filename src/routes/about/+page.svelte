d970afd35713bad3d94697ce3f75dc575ffd24f0
Muhammad Hafid K <muhammad.khoirul@mekari.com>
2023-08-24 08:08:47 +0700

    add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  let year = new Date().getFullYear();
+</script>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<div class="pb-44">
+  <h1 class="text-3xl md:text-5xl font-bold py-16 text-center">ABOUT</h1>
+  <div class="">
+    <div class="text-center text-lg">
+      <p>Hi, how are you? I hope you are fine!</p>
+      <p>My name is <span class="text-violet-600">Muhammad Hafid Khoirul</span>, people usually call me <span class="text-violet-600">Hafid</span>.</p>
+      <p class="mt-3 leading-relaxed">
+        I'm a software developer based in Bogor, Indonesia, currently working as
+        a backend engineer at <a class="text-violet-600 hover:text-violet-700 underline underline-offset-2" href="https://wwww.qontak.com" target="_blank" rel="noreferrer">Qontak</a>.
+        I have been working as a backend engineer since I graduated from Brawijaya University in 2020 with computer engineer major. So, it's been {year-2020} years, time does flies fast.
+      </p>
+      <p class="mt-3 leading-relaxed">
+        I'm focused on backend technology right now. Mainly use Ruby on Rails at the moment, tried a lot of things, Go, Phoenix, NestJs, etc, love them.
+        Beside web development, I also interested in other fields like computer vision and IoT. Doing small projects in my spare time.
+        My thesis was about computer vision by the way, you can read <a class="text-violet-600 hover:text-violet-700 underline underline-offset-2" href="https://j-ptiik.ub.ac.id/index.php/j-ptiik/article/view/7129" target="_blank" rel="noreferrer">here</a>.
+      </p>
+      <p class="mt-3 leading-relaxed">
+        I made this site to be my portfolio and blog. My writing skill needs a lot of practices... That's why I'm going to share everything I want here.
+        This will work also as my note, dont't forget everything after you learn something, take notes!
+      </p>
+      <p class="mt-3 leading-relaxed">
+        Finally, thank you for visiting this site, means a lot &nbsp;&#128156;&nbsp;
+      </p>
+    </div>
+  </div>
+</div>
